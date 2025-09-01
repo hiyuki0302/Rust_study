55008de1eb2f6f8aa52417b2e529f9fc724de251
hiyuki0302 <crypto.streeeet@gmail.com>
2025-09-01 16:52:42 +0900

    2025/09/01

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,26 +1,7 @@
-pub struct Guess {
-    value: i32,
-}
-
-// --snip--
-impl Guess {
-    pub fn new(value: i32) -> Guess {
-        if value < 1 {
-            panic!(
-                //予想値は1以上でなければなりませんが、{}でした。
-                "Guess value must be greater than or equal to 100, got {}.",
-                value
-            );
-        } else if value > 100 {
-            panic!(
-                //予想値は100以下でなければなりませんが、{}でした。
-                "Guess value must be less than or equal to 1, got {}.",
-                value
-            );
-        }
-
-        Guess { value }
-    }
+fn prints_and_returns_10(a: i32) -> i32 {
+    //{}という値を得た
+    println!("I got the value {}", a);
+    10
 }
 
 #[cfg(test)]
@@ -28,9 +9,14 @@ mod tests {
     use super::*;
 
     #[test]
-    //予想値は100以下でなければなりません
-    #[should_panic(expected = "Guess value must be less than or equal to 100")]
-    fn greater_than_100() {
-        Guess::new(200);
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10(4);
+        assert_eq!(10, value);
+    }
+
+    #[test]
+    fn this_test_will_fail() {
+        let value = prints_and_returns_10(8);
+        assert_eq!(5, value);
     }
 }
